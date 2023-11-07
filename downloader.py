61f30ad4c2695d180d02ce70d3587bd8c93ee50f
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2023-11-07 09:50:08 -0500

    Bug fixes to downloader.py - Fixed a bug where the downloader would write the processed text back to the epub, instead of creating a new txt file. - Because the arcs argument is positional, it's not possible to leave it blank. This would mean you can't simply leave it blank to download all of the arcs. I've decided to simply make it so that passing "all" in the arcs argument will download all arcs.

--- downloader.py
+++ downloader.py
@@ -8,7 +8,7 @@ import argparse
 
 
 parser = argparse.ArgumentParser(prog="sumzero-dl", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("arcs", type=str, help="The arcs to download and process. Comma separated. Leave blank to process all available arcs.", default=None)
+parser.add_argument("arcs", type=str, help="The arcs to download and process. Comma separated. Put \"all\" to process all available arcs.", default=None)
 parser.add_argument("-d", "--dir", metavar="DIR", type=str, help="Working directory (input & output) path", default="")
 parser.add_argument("-c", help="Delete the epubs after converting to txt", action="store_true")
 parser.add_argument("--dry-run", help="Don't write to the files", action="store_true")
@@ -41,17 +41,16 @@ def handle_arc(arc: int):
     for i in range(len(html)):
         html[i] = BeautifulSoup(html[i], "html.parser").text.strip()
 
-    with open(os.path.join(directory, f"Arc {arc}.epub"), "w", encoding="utf-8") as file:
+    with open(os.path.join(directory, f"Arc {arc}.txt"), "w", encoding="utf-8") as file:
         file.write("\n\n".join(html))
         
     if args.c:
         print(f"Deleting Arc {arc}.epub")
         os.remove(os.path.join(directory, f"Arc {arc}.epub"))
 
-
-if args.arcs:
-    for arc in args.arcs.split(","):
-        handle_arc(arc)
-else:
+if args.arcs == "all":
     for arc in arcs:
+        handle_arc(arc)
+elif args.arcs:
+    for arc in args.arcs.split(","):
         handle_arc(arc)
